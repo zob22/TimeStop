883cd676d8131a3cd0813ae5eafd4f1a3ed81579
Zo <65521865+zob22@users.noreply.github.com>
2023-04-01 17:53:27 +0200

    TimeStop 0.4.1
    Refactorization to make it easier to read and optimise widget creation.

--- lib/widgets/color_dropdown.dart
+++ lib/widgets/color_dropdown.dart
@@ -11,38 +11,45 @@ class ColorDropdown extends StatefulWidget {
 class _ColorDropdownState extends State<ColorDropdown> {
   @override
   Widget build(BuildContext context) {
-    return Consumer<Background>(builder: (context, background, child) {
-      return Row(
-        children: [
-          const Text('Color scheme'),
-          const Spacer(),
-          DropdownButton(
-            dropdownColor: background.selectedColor.withOpacity(0.8),
-            focusColor: background.selectedColor,
-            value: background.selectedColor,
-            items: background.colors.map((colorPreview) {
-              return DropdownMenuItem(
-                value: colorPreview,
-                child: Container(
-                  width: 120,
-                  height: 20,
-                  decoration: BoxDecoration(
-                    color: colorPreview,
-                    border: Border.all(
-                      color: Colors.grey.shade500,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(5),
+    return Consumer<Background>(
+      builder: (context, background, child) {
+        //Create a list of DropdownMenuItem widgets based on the colors defined in the background object.
+        final colorItems = [
+          for (final colorPreview in background.colors)
+            DropdownMenuItem(
+              value: colorPreview,
+              child: Container(
+                width: 120,
+                height: 20,
+                decoration: BoxDecoration(
+                  color: colorPreview,
+                  border: Border.all(
+                    color: Colors.grey.shade500,
+                    width: 1,
                   ),
+                  borderRadius: BorderRadius.circular(5),
                 ),
-              );
-            }).toList(),
-            onChanged: (value) {
-              background.setColor(value!);
-            },
-          ),
-        ],
-      );
-    });
+              ),
+            ),
+        ];
+
+        //Displays the Color Scheme setting
+        return Row(
+          children: [
+            const Text('Color scheme'),
+            const Spacer(),
+            DropdownButton(
+              dropdownColor: background.selectedColor.withOpacity(0.8),
+              focusColor: background.selectedColor,
+              value: background.selectedColor,
+              items: colorItems,
+              onChanged: (value) {
+                background.setColor(value!);
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
